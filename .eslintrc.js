6966b72170700e4a113c60f6f4661a6f2c9da4f1
Maxime <gendron.maxime1@gmail.com>
2022-11-12 00:29:21 -0500

    ESlint integration with astro

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,45 +1,46 @@
 module.exports = {
-  "parser": "@typescript-eslint/parser",
-  "overrides": [
-    {
-      "files": ['*.ts'],
-      "parserOptions": {
-        "project": [
-          './tsconfig.json'
-        ],
-      },
-    }
-  ],
-  "plugins": [
-    "@typescript-eslint",
-    "prettier"
-  ],
-  "extends": [
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking"
-  ],
-  "root": true,
-  "env": {
-    "node": true,
-    "es2021": true
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['./tsconfig.json'],
+    extraFileExtensions: ['.astro'],
   },
-  "rules": {
-    "prettier/prettier": ["error", { "endOfLine": "auto" }],
-    "@typescript-eslint/no-explicit-any": ["warn"],
-    "@typescript-eslint/no-unsafe-argument": ["warn"],
-    "@typescript-eslint/explicit-function-return-type": ["error"],
-    "brace-style": "off",
-    "@typescript-eslint/brace-style": ["error", "1tbs", { "allowSingleLine": true } ],
-    "comma-spacing": "off",
-    "@typescript-eslint/comma-spacing": ["error"],
-    "no-shadow": "off",
-    "@typescript-eslint/no-shadow": ["error", { "allow": ["err", "resolve", "reject"] } ],
-    "object-curly-spacing": "off",
-    "@typescript-eslint/object-curly-spacing": ["error", "always"],
-    "default-param-last": "off",
-    "@typescript-eslint/default-param-last": ["error"],
-    "dot-notation": "off",
-    "@typescript-eslint/dot-notation": ["error"],
-    "@typescript-eslint/explicit-member-accessibility": [ "error"]
-  }
-}
+  overrides: [
+    {
+      files: ['*.astro'],
+      parser: 'astro-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
+      },
+      rules: {
+        'astro/no-unused-css-selector': 'error',
+      },
+    },
+  ],
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:astro/recommended',
+  ],
+  root: true,
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    '@typescript-eslint/no-explicit-any': ['warn'],
+    '@typescript-eslint/no-unsafe-argument': ['warn'],
+    '@typescript-eslint/explicit-function-return-type': ['error'],
+    'brace-style': 'off',
+    '@typescript-eslint/brace-style': ['error', '1tbs', { allowSingleLine: true }],
+    'comma-spacing': 'off',
+    '@typescript-eslint/comma-spacing': ['error'],
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error', { allow: ['err', 'resolve', 'reject'] }],
+    'object-curly-spacing': 'off',
+    '@typescript-eslint/object-curly-spacing': ['error', 'always'],
+    'default-param-last': 'off',
+    '@typescript-eslint/default-param-last': ['error'],
+    'dot-notation': 'off',
+    '@typescript-eslint/dot-notation': ['error'],
+    '@typescript-eslint/explicit-member-accessibility': ['error'],
+  },
+};
