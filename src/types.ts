c1e9e5bcb0556a70ee6a794679f9665ac3729d25
Maxime <gendron.maxime1@gmail.com>
2022-11-15 18:43:40 -0500

    Index pages final integration

--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,15 @@ export interface NavItem {
   title: string;
   url: string;
 }
+
+export interface FeatureItem {
+  title: string;
+  description: string;
+  icon: string;
+}
+
+export interface FooterLink {
+  url: string;
+  description: string;
+  icon: string;
+}
