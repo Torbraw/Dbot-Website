179d1f1f01813fd3d6b1af22f731ac589d25e704
Maxime <gendron.maxime1@gmail.com>
2022-11-28 22:12:00 -0500

    Docs integration

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,11 @@
 export const CLOUDFRONT_BASE_URL = 'https://d2lqwktucnc67y.cloudfront.net/';
 export const INVITE_LINK =
   'https://discord.com/api/oauth2/authorize?client_id=804548465878237215&scope=applications.commands%20bot&permissions=1428345547856';
+
+export type SidebarContent = { text: string; link: string };
+export type Sidebar = Record<string, Record<string, SidebarContent[]>>;
+export const SIDEBAR: Sidebar = {
+  en: {
+    Dbot: [{ text: 'Introduction', link: 'intro' }],
+  },
+};
---
+++ src/utils.ts
@@ -0,0 +1,13 @@
+import { SIDEBAR, Sidebar, SidebarContent } from './config';
+
+export function paginate(
+  lang: keyof Sidebar,
+  path: string,
+): { prev: SidebarContent | undefined; next: SidebarContent | undefined } {
+  const routes = Object.values(SIDEBAR[lang]).flat();
+  const index = routes.map((item) => item.link).indexOf(path);
+  if (index === -1) return { prev: undefined, next: undefined };
+  const prev = index > 0 ? routes[index - 1] : undefined;
+  const next = index < routes.length - 1 ? routes[index + 1] : undefined;
+  return { prev, next };
+}
