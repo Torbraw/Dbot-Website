d7d22826ccc10197f31af7c931bc98df22d9a3d0
Maxime <gendron.maxime1@gmail.com>
2023-01-04 20:58:29 -0500

    feat: Changed footer discord link to community server

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 export const CLOUDFRONT_BASE_URL = 'https://d2lqwktucnc67y.cloudfront.net/';
 export const INVITE_LINK =
   'https://discord.com/api/oauth2/authorize?client_id=804548465878237215&scope=applications.commands%20bot&permissions=1428345547856';
+export const DBOT_DISCORD_LINK = 'https://discord.gg/BQbVNJfwvj';
 
 export type SidebarContent = { text: string; link: string };
 export type Sidebar = Record<string, Record<string, SidebarContent[]>>;
