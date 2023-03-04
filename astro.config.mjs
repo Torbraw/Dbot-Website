d1231000cb61b8e5092fd0d87a8629b6ec916ef0
Maxime <gendron.maxime1@gmail.com>
2023-03-04 10:32:31 -0500

    chore: Added vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from 'astro/config';
 import { loadEnv } from 'vite';
 const { SITE_URL } = loadEnv(import.meta.env.MODE, process.cwd(), '');
-
 import tailwind from '@astrojs/tailwind';
 import image from '@astrojs/image';
 import astroI18next from 'astro-i18next';
 import solidJs from '@astrojs/solid-js';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/serverless';
 
+// https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
   output: 'server',
@@ -22,4 +23,5 @@ export default defineConfig({
     mdx(),
     sitemap(),
   ],
+  adapter: vercel(),
 });
