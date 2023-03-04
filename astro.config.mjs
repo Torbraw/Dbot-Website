9f368d9e7e78a6f83cba75de8317957c808cacd8
Maxime <gendron.maxime1@gmail.com>
2023-03-04 10:12:47 -0500

    chore: .

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 import image from '@astrojs/image';
+
 import astroI18next from 'astro-i18next';
 
 // https://astro.build/config
