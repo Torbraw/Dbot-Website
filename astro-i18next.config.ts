64399dd31c11c0d154f164c405469cb3f9b04203
Maxime <gendron.maxime1@gmail.com>
2023-03-04 13:18:46 -0500

    debug

--- astro-i18next.config.ts
+++ astro-i18next.config.ts
@@ -2,11 +2,12 @@ import type { AstroI18nextConfig } from "astro-i18next";
 
 const config: AstroI18nextConfig = {
   defaultLocale: "en",
-  locales:['fr', 'en'],
+  locales: ['fr', 'en'],
   namespaces: ["index", "header", "footer", "404", "docs", "encyclopedia"],
   defaultNamespace: "index",
   i18nextServer: {
     debug: true,
+    preload: ['fr', 'en']
   },
 };
 
